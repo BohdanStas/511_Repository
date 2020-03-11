a63b19bb5a9c6bd5e9ef91cdd12250cf9fcd58ca
BIOSYSTEM <46874046+lilbios@users.noreply.github.com>
2020-03-11 11:29:28 +0200

    Added web api and console ui

---
+++ DOT_NETCore_Chat/DOT_NETCore_Chat.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DOT_NETCore_Chat.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
