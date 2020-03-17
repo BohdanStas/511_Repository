4b2d9d37b9d197813287a24a0049489c7f4ee6da
BIOSYSTEM <46874046+lilbios@users.noreply.github.com>
2020-03-17 15:37:32 +0200

    deleted ui

--- DOT_NETCore_Chat/DOT_NETCore_Chat.API/Startup.cs
+++ DOT_NETCore_Chat/DOT_NETCore_Chat.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,10 @@ namespace DOT_NETCore_Chat.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(opt=> {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
             services.AddControllers();
         }
 
